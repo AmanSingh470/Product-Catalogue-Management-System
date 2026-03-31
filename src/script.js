26dc3957f7136097f0208bb06f23925f929586f0
Aman <amansinghofficial2004@gmail.com>
2026-03-31 18:01:49 +0530

    first commit

---
+++ src/script.js
@@ -0,0 +1,86 @@
+const navbar = document.getElementById("navbar");
+const heading = document.getElementById("main-heading");
+const backImg = document.getElementById("back-img");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 1) {
+    navbar.classList.add("bg-white", "shadow-lg");
+    navbar.classList.remove("bg-transparent");
+
+    // 🔥 change heading to red
+    heading.classList.add("text-red-500");
+    heading.classList.remove("text-white");
+
+    backImg.classList.remove("bg-black/80");
+    backImg.classList.add("bg-black/60");
+    
+  } else {
+    backImg.classList.remove("bg-black/60");
+    backImg.classList.add("bg-black/80");
+
+    navbar.classList.remove("bg-white", "shadow-lg");
+    navbar.classList.add("bg-transparent");
+
+    // 🔥 back to white
+    heading.classList.remove("text-red-500");
+    heading.classList.add("text-white");
+  }
+});
+
+const gridButton = document.getElementById("grid-button");
+const listButton = document.getElementById("list-button");
+const productGrid = document.getElementById("product-grid")
+const productList = document.getElementById("product-list")
+
+gridButton.addEventListener("click", () => {
+  productList.classList.add("hidden");
+  productGrid.classList.remove("hidden");
+});
+listButton.addEventListener("click", () => {
+  productGrid.classList.add("hidden");
+  productList.classList.remove("hidden");
+});
+
+const searchInput = document.getElementById("searchInput");
+const items = document.querySelectorAll(".product-item");
+
+searchInput.addEventListener("input", () => {
+  const value = searchInput.value.toLowerCase();
+
+  items.forEach(item => {
+    const text = item.innerText.toLowerCase();
+
+    if (text.includes(value)) {
+      item.classList.remove("hidden"); // show
+    } else {
+      item.classList.add("hidden"); // hide
+    }
+  });
+});
+
+const filterButtons = document.querySelectorAll(".filter-btn");
+const contents = document.querySelectorAll(".filter-content");
+
+filterButtons.forEach(btn => {
+  btn.addEventListener("click", () => {
+
+    const target = btn.dataset.tab;
+
+    // hide all content
+    contents.forEach(c => c.classList.add("hidden"));
+
+    // show selected content
+    document.getElementById(target).classList.remove("hidden");
+
+    // reset all buttons
+    filterButtons.forEach(b => {
+      b.classList.remove("bg-black", "text-white");
+      b.classList.add("bg-white", "text-gray-600");
+    });
+
+    // activate clicked button
+    btn.classList.add("bg-black", "text-white");
+    btn.classList.remove("bg-white", "text-gray-600");
+
+  });
+});
