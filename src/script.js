8e96bba970a2fe88e434e18f18b250125dd9bdf2
Aman <amansinghofficial2004@gmail.com>
2026-04-02 16:02:59 +0530

    imporved ui

--- src/script.js
+++ src/script.js
@@ -1,29 +1,35 @@
 const navbar = document.getElementById("navbar");
 const heading = document.getElementById("main-heading");
 const backImg = document.getElementById("back-img");
+const headerAction = document.querySelectorAll(".header-action");
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 1) {
     navbar.classList.add("bg-white", "shadow-lg");
     navbar.classList.remove("bg-transparent");
 
-    // 🔥 change heading to red
+    // change heading to red
     heading.classList.add("text-red-500");
     heading.classList.remove("text-white");
-
+    
     backImg.classList.remove("bg-black/80");
     backImg.classList.add("bg-black/60");
     
+    headerAction.forEach(c => c.classList.remove("hidden"));
+
   } else {
     backImg.classList.remove("bg-black/60");
     backImg.classList.add("bg-black/80");
-
+    
     navbar.classList.remove("bg-white", "shadow-lg");
     navbar.classList.add("bg-transparent");
 
-    // 🔥 back to white
+    // back to white
     heading.classList.remove("text-red-500");
     heading.classList.add("text-white");
+
+    headerAction.forEach(c => c.classList.add("hidden"));
+
   }
 });
 
@@ -35,10 +41,22 @@ const productList = document.getElementById("product-list")
 gridButton.addEventListener("click", () => {
   productList.classList.add("hidden");
   productGrid.classList.remove("hidden");
+
+  gridButton.classList.remove('text-gray-500');
+  gridButton.classList.add('text-black');
+
+  listButton.classList.remove('text-black');
+  listButton.classList.add('text-gray-500');
 });
 listButton.addEventListener("click", () => {
   productGrid.classList.add("hidden");
   productList.classList.remove("hidden");
+
+  listButton.classList.remove('text-gray-500');
+  listButton.classList.add('text-black');
+
+  gridButton.classList.remove('text-black');
+  gridButton.classList.add('text-gray-500');
 });
 
 const searchInput = document.getElementById("searchInput");
